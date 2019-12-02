2fe90abbad3edde891871ab19e89fad7e89c54ff
Ronald Escobar <ronald.escobar@truextend.com>
2019-12-02 19:58:31 -0400

    added cardGroup. added readme

--- src/components/albums-list.js
+++ src/components/albums-list.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import Album from './album';
+import CardGroup from 'react-bootstrap/CardGroup'
 
 const AlbumsList = ({ albums }) => {
     return (
         <div>
-            {albums.map((album) => {
-                return (
-                    <Album
-                        name={album.collectionCensoredName}
-                        artist={album.artistName}
-                        price={album.collectionPrice}
-                        imageUrl={album.artworkUrl100}>
-
-                    </Album>
-                )
-            })}
+            <CardGroup>
+                {albums.map((album) => {
+                    return (
+                        <Album
+                            name={album.collectionCensoredName}
+                            artist={album.artistName}
+                            price={album.collectionPrice}
+                            imageUrl={album.artworkUrl100}>
+                        </Album>
+                    )
+                })}
+            </CardGroup>
         </div>
     );
 }
