eacd1096b7782f375fdf698a761bfd41eaa0ccfc
Ronald Escobar <ronald.escobar@truextend.com>
2019-12-02 17:01:48 -0400

    added album and albums list with bootstrap styles and temporary hardcoded data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import MainView from './components/main-view';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/main-view.js
+++ src/components/main-view.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import AlbumsList from './albums-list';
 
 class MainView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            albums: []
+            albums: [
+                {
+                    name: 'Sgt peppers',
+                    artist: 'The Beatles',
+                    price: '20$'
+                },
+                {
+                    name: 'Unknown Pleasures',
+                    artist: 'Joy Division',
+                    price: '15$'
+                },
+            ]
         }
     }
 
     render() {
         return (
-            <div>Vista principal</div>
+            <div>
+                <div>Vista principal</div>
+                <AlbumsList albums={this.state.albums}></AlbumsList>
+            </div>   
         )
     }
 }
